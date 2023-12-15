13faf6df17806db274c2fb0dac96c2076ad7ec0c
jaydip1235 <jaydipdeyalok@gmail.com>
2023-12-15 23:12:42 +0530

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/chat',
     createProxyMiddleware({
-      target: 'http://100.80.227.42:5000',
+      target: 'http://100.80.227.42:5000/chat',
       changeOrigin: true,
     })
   );
