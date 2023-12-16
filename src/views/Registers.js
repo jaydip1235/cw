4508fb78de57539156dbc05019a37be868f29bd5
jaydip1235 <jaydipdeyalok@gmail.com>
2023-12-16 13:50:56 +0530

    .

--- src/views/Registers.js
+++ src/views/Registers.js
@@ -10,8 +10,8 @@ function Registers() {
     <Col className="text-center mx-auto" style={{ maxWidth: "400px" }}>
       <div className="d-flex flex-column ms-5">
       <div className="text-center">
-             <p>Powered by</p> 
-              <img width="300px" style={{marginLeft:"10px"}} src="https://www.intel.com/content/dam/develop/public/us/en/images/thumbnails/tool-thumbnail-beta-oneapi-logo.jpg"/>
+              <img width="300px" style={{marginLeft:"10px"}} src="https://cdn.discordapp.com/attachments/1181657760228720640/1185495513743761478/image.png?ex=658fd1bb&is=657d5cbb&hm=8f8396ccbc914ba0a7af9a0e2c4479f0ac49ac9d22f46ff690efcc4ceafa8b17&"/>
+              <p className="mt-2">Powered by oneAPI</p>
               <h4 className="mt-1 mb-3 mt-5 pb-1 h3">Register</h4>
             </div>
 
